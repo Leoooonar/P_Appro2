51b6f9bfcfbc40bf1288bf7e79c93f5b415167ed
Leoooonar <143504110+Leoooonar@users.noreply.github.com>
2024-03-22 16:24:11 +0100

    Looping créé -> Création de la DB avec les nouvelles tables (t_reservation et t_places)
    Importation sur PhpMyAdmin réussie

--- ConnecteurMySQL/db_P_Appro2.sql
+++ ConnecteurMySQL/db_P_Appro2.sql
@@ -11,3 +11,23 @@ CREATE TABLE t_user (
     useIsAdmin tinyint(1) DEFAULT 0,
     PRIMARY KEY (user_id)
 );
+
+CREATE TABLE t_places (
+    place_id INT NOT NULL AUTO_INCREMENT,
+    plaType varchar(50) NOT NULL,
+    plaPrice DECIMAL(15,2) NOT NULL,
+    PRIMARY KEY (place_id)
+);
+
+CREATE TABLE t_reservation (
+    reservation_id INT NOT NULL AUTO_INCREMENT,
+    resDate DATE NOT NULL,
+    resMatin BOOLEAN NOT NULL,
+    resApresMidi BOOLEAN NOT NULL,
+    resStatut varchar(50) NOT NULL,
+    places_fk INT NOT NULL,
+    user_fk INT NOT NULL,
+    PRIMARY KEY (reservation_id),
+    FOREIGN KEY (places_fk) REFERENCES t_places(place_id),
+    FOREIGN KEY (user_fk) REFERENCES t_user(user_id)
+);
